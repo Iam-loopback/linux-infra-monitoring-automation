720aa9775c49bfd41960028a0d7edf457c13230f
sasikumar <sasikumarannathurai6326@gmail.com>
2026-05-20 14:56:02 +0530

    Update  webpage content

--- scripts/update_webpage.sh
+++ scripts/update_webpage.sh
@@ -7,11 +7,11 @@ cat <<EOF | sudo tee /var/www/html/index.html
 
 <html>
 <head>
-<title>Linux Admin Lab></title>
+<title>Linux Infrastructure Monitoring & Automation System</title>
 </head>
 <body>
-<h1>Linux Adminstration Project</h1>
-<p>This server is managed by Sasikumar</p>
+<h1>Linux Infrastructure Monitoring & Automation System</h1>
+<p>This project simulates a Linux infrastructure environment with monitoring, automation, backup, and web service managemnt.</p>
 <h2>Server Information</h2>
 <pre>
 Hostname: $HOST
